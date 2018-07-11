5c8a73cd0f4f4fc850af0601bca9906b3fe0bc50
Jerry <jerry.zhang@oocl.com>
2018-07-11 12:20:06 +0800

    finished number end of 3

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,14 +3,26 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
     public String fizzBuzz(int i) {
         StringBuffer sb = new StringBuffer();
-        if(i % 3 == 0)
+        if (i % 3 == 0 )
             sb.append("Fizz");
-        if (i%5==0) {
+        if (i % 5 == 0) {
             sb.append("Buzz");
         }
-        if (i%7==0) {
+        if (i % 7 == 0) {
             sb.append("Whizz");
         }
-        return sb.toString().equals("")?String.valueOf(i):sb.toString();
+        if (i % 10 == 3 ){
+            sb.delete(0, sb.length());
+            sb.append("Fizz");
+        }
+        if (i % 10 == 5) {
+            sb.append("Buzz");
+        }
+        if (i % 10 == 7) {
+            sb.append("Whizz");
+        }
+
+
+        return sb.toString().equals("") ? String.valueOf(i) : sb.toString();
     }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -3,57 +3,68 @@ package com.thoughtworks.tdd;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
+
+    //数字包含3只打印Fizz
     @Test
-    public void shoul_return_1_when_input_1(){
+    public void shoul_return_1_when_input_1() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(1);
-        assert(result.equals("1"));
+        assert (result.equals("1"));
     }
 
     @Test
-    public void should_return_fizz_when_input_3(){
+    public void should_return_fizz_when_input_3() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(3);
-        assert(result.equals("Fizz"));
+        assert (result.equals("Fizz"));
     }
 
     @Test
-    public void should_return_Buzz_when_input_5(){
+    public void should_return_Buzz_when_input_5() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(5);
-        assert(result.equals("Buzz"));
+        assert (result.equals("Buzz"));
     }
 
     @Test
-    public void should_return_Whizz_when_input_7(){
+    public void should_return_Whizz_when_input_7() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(7);
-        assert(result.equals("Whizz"));
+        assert (result.equals("Whizz"));
     }
+
     @Test
-    public void should_return_FizzBuzz_when_input_15(){
+    public void should_return_FizzBuzz_when_input_15() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(15);
-        assert(result.equals("FizzBuzz"));
+        assert (result.equals("FizzBuzz"));
     }
+
     @Test
-    public void should_return_FizzWhizz_when_input_21(){
+    public void should_return_FizzWhizz_when_input_21() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(21);
-        assert(result.equals("FizzWhizz"));
+        assert (result.equals("FizzWhizz"));
     }
 
     @Test
-    public void should_return_BuzzWhizz_when_input_35(){
+    public void should_return_BuzzWhizz_when_input_35() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(35);
-        assert(result.equals("BuzzWhizz"));
+        assert (result.equals("BuzzWhizz"));
     }
 
     @Test
-    public void should_return_FizzBuzzWhizz_when_input_105(){
+    public void should_return_FizzBuzzWhizz_when_input_105() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.fizzBuzz(105);
-        assert(result.equals("FizzBuzzWhizz"));
+        assert (result.equals("FizzBuzzWhizz"));
+    }
+
+    @Test
+    public void should_return_Fizz_when_input_endOf_3() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.fizzBuzz(13);
+        assert (result.equals("Fizz"));
     }
 }
