97273887bf82cd1b6480dd717d7f1d10b21ae6ac
Jerry <jerry.zhang@oocl.com>
2018-07-13 17:29:21 +0800

    test input correct order

--- src/test/java/com/thoughtworks/parkingSystem3/ParkingSystemTest.java
+++ src/test/java/com/thoughtworks/parkingSystem3/ParkingSystemTest.java
@@ -13,11 +13,23 @@ public class ParkingSystemTest {
     @Test
     public void should_return_false_when_input_is_incorrect(){
         ParkingSystem parkingSystem = new ParkingSystem();
+
         ParkingSystemIO parkingSystemIO = mock(ParkingSystemIO.class);
         when(parkingSystemIO.getOrder()).thenReturn("343");
 
         boolean order = parkingSystem.getOrder(parkingSystemIO);
         assertThat(order,is(false));
+    }
+
+    @Test
+    public void should_return_true_when_input_is_correct(){
+        ParkingSystem parkingSystem = new ParkingSystem();
+
+        ParkingSystemIO parkingSystemIO = mock(ParkingSystemIO.class);
+        when(parkingSystemIO.getOrder()).thenReturn("1");
+
+        boolean order = parkingSystem.getOrder(parkingSystemIO);
+        assertThat(order,is(true));
     }
 
 }
